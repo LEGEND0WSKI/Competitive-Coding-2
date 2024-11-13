a65345f802cf8ecc2d6d820bc5fac31b3b8d6a7c
LEGEND0WSKI <siddhesh.koli32@gmail.com>
2024-11-12 23:46:14 -0600

    2/2 checks done

--- Problem-2.py
+++ Problem-2.py
@@ -1,4 +1,4 @@
-#Knapsack with dp
+#Knapsack with dp : t:O(m*n),s:O(m*n)
 class Solution:
     def knapSack(self, capacity:int, val:list[int], wt:list[int])-> int:
 
@@ -7,7 +7,7 @@ class Solution:
         
         # dp array
         dp = [[0]*(n+1) for i in range(m+1)] #init dp
-        # // base c
+        # // base case
         #not needed since all rows are 0
         # // logic
         for i in range(1,m+1):              
@@ -15,26 +15,26 @@ class Solution:
                 if j < wt[i-1]:                                                 # if current capacity less than weight of item // i-1 since (1,m+1),(1,n+1)                        
                     dp[i][j] = dp[i-1][j]                                       # no choose case
                 else:
-                    dp[i][j]  = max(dp[i-1][j], val[i-1]+dp[i-1][j-wt[i-1]])    # choose case 
+                    dp[i][j]  = max(dp[i-1][j], val[i-1]+dp[i-1][j-wt[i-1]])    # choose case // j-wt[i] -> current capacity - weight[current]
 
         return dp[m][n]
     
 print(Solution().knapSack(capacity = 4,val = [1, 2, 3,],wt =[4,5,1])) #op = 3
 
-# recursive helper
+# recursive helper: t:O(2^n) for recursion and O(n) for stack space
 # class Solution:
 #     def knapSack(self, capacity, val, wt):
 #             # code here
 #         return self.helper(capacity,wt,val,0,0)
         
 #     def helper(self,capacity,wt,val,i,totalProfit):
-#         if i >=len(wt):
+#         if i >=len(wt):                       #if index goes beyond lenght of weight array return final profit
 #             return totalProfit
             
 #         case0 = self.helper(capacity,wt,val,i+1,totalProfit)
             
 #         case1 = 0
-#         if wt[i] <=capacity:
+#         if wt[i] <=capacity:                  # if item can fit in given capcity
 #             case1 = self.helper(capacity-wt[i],wt,val,i+1,totalProfit+val[i])
             
 #         return max(case0,case1)
--- problem1.py
+++ problem1.py
@@ -1,14 +1,15 @@
+#2sum t:O(n) and s:O(1)
 class Solution:
     def twosum(self,nums:list[int], target:int):
-        seen = {}
+        seen = {}                           # we create a hashmap of seen items
         
-        for i in range(len(nums)):
-            diff = target - nums[i]
+        for i in range(len(nums)):  
+            diff = target - nums[i]         # find diffrence  
 
-            if diff in seen:
+            if diff in seen:                # if found -> output 
                 return(i, seen[diff])
             else:
-                seen[nums[i]] = i
+                seen[nums[i]] = i           # store key = nums[i] and value = index in the Hashmap 
 
 
 
